fd38289c42e5ae8e750c2524ae47471d6b7fa352
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-07-07 13:03:45 +0100

    add data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,9 @@
-_## code to prepare `DATASET` dataset goes here
+## code to prepare `DATASET` dataset goes here
 
 # usethis::use_data(DATASET, overwrite = TRUE)
 
 library(readxl)
 library(tidyverse)
+library(here)
+
+df_raw <- read_excel(here("data-raw/"))
