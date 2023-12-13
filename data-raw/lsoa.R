c8d17ecbf78cfcf132cbd84bd6f2affcdb4576e6
Lextuga007 <zoe.turner3@nhs.net>
2023-12-13 16:47:44 +0000

    Used styler on package

--- data-raw/lsoa.R
+++ data-raw/lsoa.R
@@ -11,17 +11,21 @@ library(janitor)
 # LSOA 2019 scores ----
 ## Estimates for male
 df_raw_m <- read_excel(here("data-raw/SAPE22DT2-mid-2019-lsoa-syoa-estimates-unformatted.xlsx"),
-                       sheet = "Mid-2019 Males",
-                       range = "A5:CT34758")
+  sheet = "Mid-2019 Males",
+  range = "A5:CT34758"
+)
 
 ## Estimates for female
 df_raw_f <- read_excel(here("data-raw/SAPE22DT2-mid-2019-lsoa-syoa-estimates-unformatted.xlsx"),
-                       sheet = "Mid-2019 Females",
-                       range = "A5:CT34758")
+  sheet = "Mid-2019 Females",
+  range = "A5:CT34758"
+)
 
 # Create vector with better names
-rename_age_vars <- setNames(paste0("x", 0:90),
-                            c(paste0("age", 0:89), "age90plus"))
+rename_age_vars <- setNames(
+  paste0("x", 0:90),
+  c(paste0("age", 0:89), "age90plus")
+)
 
 # Rename both data objects
 df_f_wide <- df_raw_f %>%
@@ -34,39 +38,49 @@ df_m_wide <- df_raw_m %>%
 
 # Pivot data longer
 df_f_long <- df_f_wide %>%
-  pivot_longer(cols = all_of(c(paste0("age", 0:89), "age90plus")),
-               names_to = "age",
-               values_to = "n") %>%
+  pivot_longer(
+    cols = all_of(c(paste0("age", 0:89), "age90plus")),
+    names_to = "age",
+    values_to = "n"
+  ) %>%
   mutate(age = as.numeric(str_extract(age, "\\d+"))) %>%
   rename(n_all_ages_female = all_ages) %>%
   select(lsoa_code,
-         lsoa_name,
-         la_code = la_code_2019_boundaries,
-         la_name = la_name_2019_boundaries,
-         age,
-         n) %>%
-  mutate(lsoa_year = 2019,
-         la_year = 2019,
-         est_year = 2019,
-         gender = "f") %>%
+    lsoa_name,
+    la_code = la_code_2019_boundaries,
+    la_name = la_name_2019_boundaries,
+    age,
+    n
+  ) %>%
+  mutate(
+    lsoa_year = 2019,
+    la_year = 2019,
+    est_year = 2019,
+    gender = "f"
+  ) %>%
   relocate(lsoa_year, lsoa_code, lsoa_name, la_year, la_code, la_name, age, gender, est_year, n)
 
 df_m_long <- df_m_wide %>%
-  pivot_longer(cols = all_of(c(paste0("age", 0:89), "age90plus")),
-               names_to = "age",
-               values_to = "n") %>%
+  pivot_longer(
+    cols = all_of(c(paste0("age", 0:89), "age90plus")),
+    names_to = "age",
+    values_to = "n"
+  ) %>%
   mutate(age = as.numeric(str_extract(age, "\\d+"))) %>%
   rename(n_all_ages_male = all_ages) %>%
   select(lsoa_code,
-         lsoa_name,
-         la_code = la_code_2019_boundaries,
-         la_name = la_name_2019_boundaries,
-         age,
-         n) %>%
-  mutate(lsoa_year = 2019,
-         la_year = 2019,
-         est_year = 2019,
-         gender = "m") %>%
+    lsoa_name,
+    la_code = la_code_2019_boundaries,
+    la_name = la_name_2019_boundaries,
+    age,
+    n
+  ) %>%
+  mutate(
+    lsoa_year = 2019,
+    la_year = 2019,
+    est_year = 2019,
+    gender = "m"
+  ) %>%
   relocate(lsoa_year, lsoa_code, lsoa_name, la_year, la_code, la_name, age, gender, est_year, n)
 
 # Join data and arrange by code
@@ -75,4 +89,3 @@ lsoa <- df_f_long %>%
   arrange(lsoa_year, lsoa_code, est_year, gender)
 
 usethis::use_data(lsoa, overwrite = TRUE)
-
