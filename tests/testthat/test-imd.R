c3ddcbcd2eb0c5162d7e452139d1d52560a03531
Lextuga007 <zoe.turner3@nhs.net>
2023-12-22 15:13:08 +0000

    Added test for only one IMD returned as postcodes are not all valid

--- tests/testthat/test-imd.R
+++ tests/testthat/test-imd.R
@@ -16,3 +16,20 @@ testthat::test_that("Only one line per postcode is returned", {
     length(postcodes), n_rows
   )
 })
+
+
+testthat::test_that("Only one IMD score is returned because only 1 postcode
+                    is valid", {
+  valid_imd <- 1
+
+  test_vector <- get_imd(postcodes)
+  test_df <- get_imd(test_df1)
+
+  testthat::expect_equal(
+    sum(!is.na(test_vector$imd_decile)), valid_imd
+  )
+
+  testthat::expect_equal(
+    sum(!is.na(test_df$imd_decile)), valid_imd
+  )
+})
