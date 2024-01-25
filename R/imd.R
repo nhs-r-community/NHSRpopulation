ef6193f0e044fd030f4dce1761ae0ac9e6b846bf
Lextuga007 <zoe.turner3@nhs.net>
2024-01-25 17:30:17 +0000

    Removed stray example from another package

--- R/imd.R
+++ R/imd.R
@@ -15,12 +15,6 @@
 #' on the totals.
 #'
 #' @examples
-#' postcodes <- c("HD1 2UT", "HD1 2UU", "HD1 2UV")
-#' test_df1 <- dplyr::tibble(
-#'   place = paste0("place_", 1:3),
-#'   postcode = postcodes
-#' )
-#' postcode_data_join(test_df1, fix_invalid = TRUE)
 #' @export
 get_imd <- function(
     .data,
