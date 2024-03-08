02ccf8a9ab46a401daf4d90820926c4dbdbe0a8c
Lextuga007 <zoe.turner3@nhs.net>
2024-03-08 21:56:54 +0000

    Moved into specific functions

---
+++ R/get_data.R
@@ -0,0 +1,163 @@
+#' Getting data from the api_urls
+#'
+#' @param url_type String defaults to `postcode` which connects to
+#' Online_ONS_Postcode_Directory_Live and `imd` connects to
+#' Indices_of_Multiple_Deprivation_(IMD)_2019
+#'
+#' @return dataset
+#' @export
+api_url <- function(url_type = c(
+                      "postcode",
+                      "imd",
+                      "lsoa"
+                    )) {
+  url_type <- match.arg(url_type)
+
+  if (url_type == "postcode" | url_type == "lsoa") {
+    api_url <- paste0(
+      "https://services1.arcgis.com/ESMARspQHYMw9BZ9/ArcGIS/",
+      "rest/services/Online_ONS_Postcode_Directory_Live/",
+      "FeatureServer/0/query"
+    )
+  }
+
+  if (url_type == "imd") {
+    api_url <- paste0(
+      "https://services3.arcgis.com/ivmBBrHfQfDnDf8Q/arcgis/rest/services/",
+      "Indices_of_Multiple_Deprivation_(IMD)_2019/FeatureServer/0/query"
+    )
+  }
+
+  req <- httr2::request(api_url) |>
+    httr2::req_url_query(f = "json")
+
+  req
+}
+
+#' Query information to restrict data returned
+#'
+#' @description
+#' This function queries the API based on the information required and returns
+#' ids as these have no restriction applied to them.
+#' The IMD API restricts to 2k records for example.
+#'
+#' Postcode and LSOA parameters require data because bringing all the data
+#' from the Online_ONS_Postcode_Directory_Live will take too long and is often
+#' unnecessary.
+#'
+#' @param url_type String "postcode" and "lsoa" connect to
+#' Online_ONS_Postcode_Directory_Live and imd connects to
+#' Indices_of_Multiple_Deprivation_(IMD)_2019
+#' @param data String in "postcode" this is checked using the
+#' {NHSRpostcodetools} so selects the postcode column from a data frame or can
+#' handle a vector.
+#' This functionality needs to be extended to "lsoa" codes which currently only
+#' allow a vector.
+#' "imd" brings all data back and only relates to the English IMD.
+#'
+#' @return list Ids only as these are faster to batch
+#' @export
+#'
+get_data <- function(data,
+                     url_type = c(
+                       "postcode",
+                       "imd",
+                       "lsoa"
+                     )) {
+  url_type <- match.arg(url_type)
+
+  req <- api_url(url_type)
+
+  # Check the data frame or vector for any postcode to then run through
+  # the postcode_data_join API
+  if (sum(is_postcode(as.vector(t(test_df1))), na.rm = TRUE) > 0) {
+    data <- NHSRpostcodetools::postcode_data_join(x = data)
+
+  }
+
+  if (url_type == "postcode") {
+    text <- paste0(
+      "PCDS IN ('",
+      paste(data$new_postcode,
+        collapse = "', '"
+      ), "')"
+    )
+  }
+
+  if (url_type == "lsoa") {
+    text <- paste0(
+      "LSOA11 IN ('",
+      paste(data$lsoa11,
+        collapse = "', '"
+      ), "')"
+    )
+  }
+
+  if (url_type == "imd") {
+    # text <- "1=1" # get all rows (no filter) Takes a while to run
+    text <- paste0(
+      "LSOA11CD IN ('",
+      paste(data$lsoa11,
+        collapse = "', '"
+      ), "')"
+    )
+  }
+
+  ids <- req |>
+    httr2::req_url_query(returnIdsonly = TRUE) |>
+    httr2::req_url_query(where = text) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json() |>
+    purrr::pluck("objectIds")
+
+  ids_batched <- NHSRpostcodetools::batch_it(ids, 100L)
+
+  # Uses function retrieve data
+  poss_retrieve_data <- purrr::possibly(retrieve_data) # safely handle any errors
+
+  resps <- ids_batched |>
+    purrr::map(\(x) poss_retrieve_data(req, x)) |>
+    purrr::compact()
+
+  # Uses function pull_table_data
+  poss_pull_table_data <- purrr::possibly(pull_table_data)
+
+  data_out <- resps |>
+    purrr::map(poss_pull_table_data) |>
+    purrr::list_rbind()
+
+  data_out
+}
+
+#' use batched IDs to retrieve table data
+#'
+#' @param req used in function \code{\link{get_data}}
+#' @param ids_vec used in function \code{\link{get_data}}
+#'
+#' @return function
+#' @export
+retrieve_data <- function(req, ids_vec) {
+  ids <- stringr::str_flatten(ids_vec, collapse = ",")
+  req |>
+    httr2::req_url_query(objectIds = ids) |>
+    httr2::req_url_query(outFields = "*") |> # returns all columns
+    httr2::req_url_query(returnGeometry = FALSE) |> # superfluous tbf
+    httr2::req_retry(max_tries = 3) |> # shouldn't be needed
+    httr2::req_perform()
+}
+
+
+#' pull actual data out from API JSON response
+#'
+#' @param respused in function \code{\link{get_data}}
+#'
+#' @return function
+#' @export
+pull_table_data <- function(resp) {
+  resp |>
+    httr2::resp_check_status() |>
+    httr2::resp_body_json() |>
+    purrr::pluck("features") |>
+    purrr::map_df("attributes") |>
+    janitor::clean_names()
+}
---
+++ tests/testthat/test-get_data.R
@@ -0,0 +1,113 @@
+
+postcodes <- c("HD1 2UT", "HD1 2UU", "HD1 2UV")
+
+imd <- c("E01011107", "E01011229")
+
+test_df1 <- dplyr::tibble(
+  place = paste0("place_", 1:3),
+  lsoa11 = c("E01011107",
+             "E01011229", "E01011229"),
+  postcode = postcodes
+)
+
+# Taken from
+# www.gov.uk/government/statistics/english-indices-of-deprivation-2019
+
+postcodes_from_each_decile <- c(
+  "E01000002",
+  "E01000001",
+  "E01000117",
+  "E01000119",
+  "E01000069",
+  "E01000070",
+  "E01000066",
+  "E01000005",
+  "E01000008",
+  "E01000048"
+)
+
+
+# get_data() ----------------------------------------------------------------
+
+httptest2::with_mock_dir("postcodes", {
+  test_that("Returns the default and url_type = 'postcode'", {
+    n_rows <- 3
+    n_col <- 41
+
+    testthat::expect_equal(
+      nrow(get_data(test_df1)), n_rows
+    )
+
+    testthat::expect_equal(
+      nrow(get_data(postcodes)), n_rows
+    )
+
+    testthat::expect_equal(
+      ncol(get_data(test_df1)), n_col
+    )
+
+    testthat::expect_equal(
+      ncol(get_data(postcodes)), n_col
+    )
+
+    testthat::expect_equal(
+      nrow(get_data(test_df1, url_type = "postcode")), n_rows
+    )
+
+    testthat::expect_equal(
+      nrow(get_data(postcodes, url_type = "postcode")), n_rows
+    )
+
+    testthat::expect_equal(
+      ncol(get_data(test_df1, url_type = "postcode")), n_col
+    )
+
+    testthat::expect_equal(
+      ncol(get_data(postcodes, url_type = "postcode")), n_col
+    )
+
+  })
+})
+
+
+httptest2::with_mock_dir("imd", {
+  test_that("Returns url_type = 'imd'", {
+    n_rows <- 3
+    n_col <- 66
+
+    # [TODO] only showing what connects to
+    testthat::expect_equal(
+      nrow(get_data(test_df1, url_type = "imd")), n_rows
+    )
+
+    # testthat::expect_equal(
+    #   nrow(get_data(imd, url_type = "imd")), n_rows
+    # )
+
+    testthat::expect_equal(
+      ncol(get_data(test_df1, url_type = "imd")), n_col
+    )
+
+    # testthat::expect_equal(
+    #   ncol(get_data(imd, url_type = "imd")), n_col
+    # )
+
+  })
+})
+
+
+testthat::test_that("Returned IMD decile", {
+  testthat::expect_equal(
+    get_data(test_df1, url_type = "imd") |>
+      dplyr::filter(!is.na(imd_decile)) |>
+      dplyr::select(imd_decile) |>
+      dplyr::pull(), 3
+  )
+
+  testthat::expect_equal(
+    get_imd(postcodes) |>
+      dplyr::filter(!is.na(imd_decile)) |>
+      dplyr::select(imd_decile) |>
+      dplyr::pull(), 3
+  )
+})
